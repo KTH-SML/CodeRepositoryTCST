a3eebc74287568c2dfbaa1c4d8fab25dc71bfd63
Kuba <kuba.nowak1993@gmail.com>
2018-03-06 14:56:16 +0100

    Node for robot simulation

---
+++ src/robot_node.cpp
@@ -0,0 +1,54 @@
+#include "ros/ros.h"
+#include "hybrid_controller/ControlInput.h"
+#include "geometry_msgs/PoseStamped.h"
+
+class Robot{
+	ros::Publisher pose_pub;
+	ros::Subscriber control_sub;
+	//std::vector<double> u;
+	double x, y, theta, t_prev;
+public:
+	Robot(ros::NodeHandle nh){
+		t_prev = ros::Time::now().toSec();
+		x = y = theta = 0.0;
+		control_sub = nh.subscribe("/control_input", 100, &Robot::controlCallback, this);
+		pose_pub = nh.advertise<geometry_msgs::PoseStamped>("/pose", 100);
+	}
+	
+	void controlCallback(const hybrid_controller::ControlInput::ConstPtr& msg){
+		double t = ros::Time::now().toSec();
+		double dt = t - t_prev;
+		t_prev = t;
+
+		x += msg->vx*dt;
+		y += msg->vy*dt;
+		theta += msg->w*dt;
+	}
+
+	void publish(){
+		geometry_msgs::PoseStamped pose_stamped;
+        pose_stamped.header.stamp = ros::Time::now();
+        pose_stamped.header.frame_id = "odom";
+        pose_stamped.pose.position.x = x;
+        pose_stamped.pose.position.y = y;
+        pose_stamped.pose.orientation.z = sin(theta*0.5);
+        pose_stamped.pose.orientation.w = cos(theta*0.5);
+		pose_pub.publish(pose_stamped);
+		//ROS_INFO("%s, %f", ros::this_node::getName().c_str(), pose_stamped.pose.position.x);
+	}
+};
+
+int main(int argc, char* argv[]){
+	ros::init(argc, argv, "robot_node");
+	ros::NodeHandle nh;
+	Robot r(nh);
+	
+	ros::Rate rate(100);
+	while(nh.ok()){
+		ros::spinOnce();
+
+		r.publish();
+
+		rate.sleep();
+	}
+}
