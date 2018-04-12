c4119ac0ba4e43d7bccddec171c29e395ae87abb
LukSchoenbaechler <lukasch@student.ethz.ch>
2018-04-12 08:42:06 +0200

    fixed tf q m

--- rqt_simulation/src/rqt_simulation/QualisysMapTf.py
+++ rqt_simulation/src/rqt_simulation/QualisysMapTf.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 import roslib
+import os
+import yaml
+import codecs
+import rospkg
 import numpy
 import Queue
 import rospy
 import sys
 import time
 import tf
+from math import sqrt
 
 from geometry_msgs.msg import PoseStamped, Transform, Pose
 
@@ -16,26 +21,36 @@ class QualisysMapTfNode(object):
     def __init__(self):
         self.node_name = "Qualisys to Map TF"
 
+        model_file = os.path.join(rospkg.RosPack().get_path('rqt_simulation'), 'config', 'qualisys_models.yaml')
+        stream = file(model_file, 'r')
+        data = yaml.load(stream)
+        models = data['models']
+
+        print(models)
+
         self.tf_qualisys_map = Transform()
         self.tf_qualisys_map.translation.x = 0.111
         self.tf_qualisys_map.translation.y = 0.105
         self.tf_qualisys_map.translation.z = 0.0
-        self.tf_qualisys_map.rotation.w = 0.0
+        self.tf_qualisys_map.rotation.w = 0.934
         self.tf_qualisys_map.rotation.x = 0.0
-        self.tf_qualisys_map.rotation.y = 0.358
-        self.tf_qualisys_map.rotation.z = 0.934
+        self.tf_qualisys_map.rotation.y = 0.0
+        self.tf_qualisys_map.rotation.z = 0.358
 
-        self.sub_qualysis_pose = rospy.Subscriber('nexus1/pose', PoseStamped, self.pose_cb)
+        self.publisher_dict = {}
 
-        self.pub_map_pose = rospy.Publisher('nexus1/map_pose', PoseStamped, queue_size = 1)
+        for i in range(0, len(models)):
+            self.sub_qualysis_pose = rospy.Subscriber(models[i] + '/pose', PoseStamped, self.pose_cb, models[i])
+            self.publisher_dict.update({models[i] : rospy.Publisher(models[i] + '/map_pose', PoseStamped, queue_size = 1)})
 
-    def pose_cb(self, msg):
+    def pose_cb(self, msg, source):
         tf_br = tf.TransformBroadcaster()
         M_pose_M_R = PoseStamped()
         M_pose_M_R.header = msg.header
         M_pose_M_R.pose = self.convert_pose_from_frame1_to_frame2(msg.pose, self.tf_qualisys_map)
-        self.pub_map_pose.publish(M_pose_M_R)
-        tf_br.sendTransform((M_pose_M_R.pose.position.x, M_pose_M_R.pose.position.y,M_pose_M_R.pose.position.z), (M_pose_M_R.pose.orientation.w, M_pose_M_R.pose.orientation.x, M_pose_M_R.pose.orientation.y, M_pose_M_R.pose.orientation.z), rospy.Time.now(), 'robot1/qualisys_odom', 'nexus1')
+        #self.pub_map_pose.publish(M_pose_M_R)
+        self.publisher_dict[source].publish(M_pose_M_R)
+        tf_br.sendTransform((M_pose_M_R.pose.position.x, M_pose_M_R.pose.position.y,M_pose_M_R.pose.position.z), (M_pose_M_R.pose.orientation.w, M_pose_M_R.pose.orientation.x, M_pose_M_R.pose.orientation.y, M_pose_M_R.pose.orientation.z), rospy.Time.now(), source + '/qualisys_odom', source)
         #print('now')
 
 
@@ -46,14 +61,17 @@ class QualisysMapTfNode(object):
         trans_1_2 = Quaternion([0.0, tf_frame1_to_frame2.translation.x, tf_frame1_to_frame2.translation.y, tf_frame1_to_frame2.translation.z])
         #print(trans_1_2)
         quat_1_2 = Quaternion([tf_frame1_to_frame2.rotation.w, tf_frame1_to_frame2.rotation.x, tf_frame1_to_frame2.rotation.y, tf_frame1_to_frame2.rotation.z])
-        print(quat_1_2)
+        #print(quat_1_2)
 
         #tf_frame2_to_frame1 = self.invert_tf(tf_frame1_to_frame2)
         quat_2_1 = quat_1_2.inverse
+        #print(quat_2_1)
 
-        trans_2_pose = quat_2_1 * (-trans_1_pose) * quat_2_1.inverse
+        trans_2_pose = quat_2_1 * trans_1_pose * quat_2_1.inverse - quat_2_1 * trans_1_2 * quat_2_1.inverse
         #print(trans_2_pose)
         quat_2_pose = quat_2_1 * quat_1_pose
+
+        #print(quat_2_pose)
 
         pose_2_pose = Pose()
         pose_2_pose.position.x = trans_2_pose[1]
