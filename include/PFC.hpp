ab7222dfdd58fb11acfd2fe8bc68cd08898b8df3
Kuba <kuba.nowak1993@gmail.com>
2018-04-24 15:45:39 +0200

    Obstacle avoidance

--- include/PFC.hpp
+++ include/PFC.hpp
@@ -2,8 +2,9 @@
 
 class PFC{
     int robot_id;
-    double robot_radius;
+    double r, R, k;
+    arma::vec u_max;
 public:
-    PFC(int robot_id, double robot_radius);
+    PFC(int robot_id, double r, double R, arma::vec u_max);
     arma::vec u(arma::vec X);
 };
