1c31f9bb95b9a769c317be00bc736070e431b064
Kuba <kuba.nowak1993@gmail.com>
2018-05-10 18:52:17 +0200

    Simplify plotting, now all data is sent from the controller, there is not need for any computations here

--- src/plotter_node.py
+++ src/plotter_node.py
@@ -16,8 +16,12 @@ class Plotter:
 		self.critical_events = []
 		self.t_ce = []
 		self.robustness = []
+		self.gamma = []
+		self.r = []
+		self.rho_max = []
 		self.t = []
-		self.t_rel = []
+		self.t_rel_t0 = []
+		self.t_rel_tr = []
 		self.robot = rospy.get_param('~robot')
 		self.u = []
 		self.uppc = []
@@ -45,74 +49,45 @@ class Plotter:
 	def robustnessCallback(self, msg):
 		self.robustness.append(msg.rho)
 		self.t.append(msg.stamp.to_sec())
-		self.t_rel.append(msg.t_relative)
+		self.rho_max.append(msg.rho_max)
+		self.r.append(msg.r)
+		self.gamma.append(msg.gamma)
+		self.t_rel_t0.append(msg.t_relative_t0)
+		self.t_rel_tr.append(msg.t_relative_tr)
 
 	def plotRobustness(self):
 		t = np.array(self.t)
 		tt = t - t[0]
  
-		gamma = np.zeros((tt.shape[0],))
-		r = np.zeros((tt.shape[0],))
-		rho_max = np.zeros((tt.shape[0],))
-		t_rel = np.array(self.t_rel)
+		gamma = np.array(self.gamma)
+		r = np.array(self.r)
+		rho_max = np.array(self.rho_max)
+		t_rel = np.array(self.t_rel_t0)
+		t_rel_tr = np.array(self.t_rel_tr)
 		g = np.zeros((tt.shape[0],))
 
-		tc = np.array(self.t_ce)
-		c = self.critical_events[0]
-		for i in range(tc.shape[0]):
-			tc[i] = t_rel[np.where(t>self.t_ce[i])[0][0]]
-
-		gamma[t_rel<=tc[0]] = -(c.gamma_0[0]-c.gamma_inf[0])*np.exp(-c.l[0]*t_rel[t_rel<=tc[0]])-c.gamma_inf[0]+c.rho_max[0]
-		r[t_rel<=tc[0]] = c.r[0]
-		rho_max[t_rel<=tc[0]] = c.rho_max[0]
-		g[t_rel<=tc[0]] = (c.gamma_0[0]-c.gamma_inf[0])*np.exp(-c.l[0]*t_rel[t_rel<=tc[0]])+c.gamma_inf[0]
-		for i in range(tc.shape[0]):
-			c = self.critical_events[i]
-			gamma[t_rel>tc[i]] = -(c.gamma_0[1]-c.gamma_inf[1])*np.exp(-c.l[1]*t_rel[t_rel>tc[i]])-c.gamma_inf[1]+c.rho_max[1]
-			r[t_rel>tc[i]] = c.r[1]
-			rho_max[t_rel>tc[i]] = c.rho_max[1]
-			g[t_rel>tc[i]] = (c.gamma_0[1]-c.gamma_inf[1])*np.exp(-c.l[1]*t_rel[t_rel>tc[i]])+c.gamma_inf[1]
-
 		fig = plt.figure()
-		plt.plot(t_rel, self.robustness, color='blue')
-
 		ax = plt.axes()
 		ax.minorticks_on()
 		ax.grid(which='major')
 		ax.grid(which='minor', alpha=0.4)
-		ax.set_ylim([-3, 0.6])
-		
-		#for c in self.critical_events:
-		#	plt.plot(t_rel, -(c.gamma_0[0]-c.gamma_inf[0])*np.exp(-c.l[0]*t_rel)-c.gamma_inf[0]+c.rho_max[0], color='black', linestyle='dashed', linewidth='0.5')
-		#plt.plot(t_rel, -(c.gamma_0[1]-c.gamma_inf[1])*np.exp(-c.l[1]*t_rel)-c.gamma_inf[1]+c.rho_max[1], color='black', linestyle='dashed', linewidth='0.5')
-
-		plt.plot(t_rel, gamma, color='green')
+		ax.set_ylim([-4, 0.6])
+		plt.plot(t_rel, self.robustness, color='blue')
+		plt.plot(t_rel, -gamma+rho_max, color='green')
 		plt.plot(t_rel, r, color='red')
 		plt.plot(t_rel, rho_max, color='green')
 		fig.savefig(os.path.dirname(os.path.dirname(__file__))+'/rho'+str(self.robot)+'.png')
 
-		fig2 = plt.figure()
-		ax = plt.axes()
-		ax.minorticks_on()
-		ax.grid(which='major')
-		ax.grid(which='minor', alpha=0.4)
 		vx = np.array(list(u.linear.x for u in self.u))
 		vy = np.array(list(u.linear.y for u in self.u))
 		vx2 = np.square(vx)
 		vy2 = np.square(vy)
 		v = np.sqrt(vx2+vy2)
-		plt.plot(t_rel, v)
-		fig2.savefig(os.path.dirname(os.path.dirname(__file__))+'/u'+str(self.robot)+'.png')
+		plotSignal(t_rel, v, self.robot, 'u')
 
 		rho = np.array(self.robustness)
 		e = (rho - rho_max)/g
-		fig3 = plt.figure()
-		ax = plt.axes()
-		ax.minorticks_on()
-		ax.grid(which='major')
-		ax.grid(which='minor', alpha=0.4)
-		plt.plot(t_rel, e)
-		fig3.savefig(os.path.dirname(os.path.dirname(__file__))+'/e'+str(self.robot)+'.png')
+		plotSignal(t_rel, e, self.robot, 'e')
 
 		uppcx = np.array(list(u.linear.x for u in self.uppc))
 		uppcy = np.array(list(u.linear.y for u in self.uppc))
