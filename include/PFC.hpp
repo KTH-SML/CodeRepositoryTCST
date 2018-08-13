2bc341efc75b9c8d6545cf8f504add545f3012da
Kuba <kuba.nowak1993@gmail.com>
2018-08-13 16:51:50 +0200

    Update configuration files for various scenarios

--- include/PFC.hpp
+++ include/PFC.hpp
@@ -2,9 +2,9 @@
 
 class PFC{
     int robot_id;
-    double r, R, k;
+    double r, R, k, w;
     arma::vec u_max;
 public:
-    PFC(int robot_id, double r, double R, arma::vec u_max);
+    PFC(int robot_id, double r, double R, arma::vec u_max, double w);
     arma::vec u(arma::vec X);
 };
